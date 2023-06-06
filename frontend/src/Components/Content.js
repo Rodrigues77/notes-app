f1ec584114d0c8097e927083b6b19da328403478
Rodrigues77 <rodrigues77@hotmail.com>
2023-06-05 23:26:37 -0300

    FIX: Quando a tela é pequena o layout é corrido impedindo sobreposição de conteúdo.

--- frontend/src/Components/Content.js
+++ frontend/src/Components/Content.js
@@ -1,23 +1,13 @@
 import { Grid } from '@mui/material';
 import * as React from 'react';
+import '../index.css';
 import NoteForm from './NoteForm';
 import Notes from './Notes';
 
 export default function Content() {
   return (
     <Grid container sx={{ minHeight: '100vh' }}>
-      {/* Esse estilo é uma adaptação técnica emergencial para fazer a nota a ser criada sempre ficar à esquerda.*/}
-      <Grid
-        item
-        xs={12}
-        md={6}
-        style={{
-          position: 'sticky',
-          position: '-webkit-sticky' /* Safari */,
-          top: 0,
-          height: '1px'
-        }}
-      >
+      <Grid item xs={12} md={6} className={'sticky-noteForm'}>
         <NoteForm />
       </Grid>
       <Grid item xs={12} md={6}>
